7374ac3b9a1d6896e1b8c322f17cd8c38bb4df3e
Tyler Long <tyler4long@gmail.com>
2021-08-27 11:21:39 -0700

    useEffect

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 
 function Example() {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
+
   return (
     <div>
       <p>You clicked {count} times</p>
